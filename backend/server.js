18263f060c2b12f077977c4b65a49b0b1f485b78
Sameer Gautam <102874030+shribreeze@users.noreply.github.com>
2023-11-01 21:47:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server = app.listen(PORT, console.log("Server started on PORT 5000..."));
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://tantu.onrender.com/",
     //   credentials: true,
     },
   });
